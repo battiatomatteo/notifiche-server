3a6231e7868d76166ee2564423267052e0c6c0bc
Battiato Matteo <99268410+battiatomatteo@users.noreply.github.com>
2025-06-08 16:06:16 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
+
+app.use(cors()); 
 app.use(express.json());
 
 app.post('/notifica', async (req, res) => {
