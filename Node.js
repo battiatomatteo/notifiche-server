6bf5469aad79c0bbcacbd940da43da8cd54653f0
Battiato Matteo <99268410+battiatomatteo@users.noreply.github.com>
2025-12-17 16:26:36 +0100

    Update Node.js

--- Node.js
+++ Node.js
@@ -29,7 +29,7 @@ app.post('/notifica', async (req, res) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Basic os_v2_app_fgbn3gdgofceleyweuwuwnlemibcjyycwyuu7kftethuacltfpd4jnkkzay5zcho63bdcjuay665pr2vneqc6b7vedv4pg7ihhuvxey'
+        'Authorization': 'Basic os_v2_app_fgbn3gdgofceleyweuwuwnlemjtvgbsqv3bu6i4zcoi4nvmixevfqihh4lhlh4ba3j6bwc7cbsfruplx57xd4ujirsv72bymxkhhmti'
       },
       body: JSON.stringify({
         app_id: '2982dd98-6671-4445-9316-252d4b356462',
